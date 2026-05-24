e90a6d5d78bb80aadcaf104f8495f28ce8844e6d
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-05-24 00:35:48 -0400

    fix api back

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ const runtimeEnv = typeof process !== 'undefined'
     ? process.env.NEXT_PUBLIC_API_GATEWAY_URL || process.env.EXPO_PUBLIC_API_GATEWAY_URL || process.env.REACT_APP_API_BASE
     : undefined;
 
-export const API_BASE = runtimeEnv || 'http://localhost:8080';
+export const API_BASE = runtimeEnv || 'http://10.0.128.152:8080';
