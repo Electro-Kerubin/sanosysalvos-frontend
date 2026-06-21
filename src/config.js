fec1f10bd945728156b92154f35125857b48a6a0
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-06-21 19:04:33 -0400

    fix: quintando el api

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ const runtimeEnv = typeof process !== 'undefined'
     ? process.env.NEXT_PUBLIC_API_GATEWAY_URL || process.env.EXPO_PUBLIC_API_GATEWAY_URL || process.env.REACT_APP_API_BASE
     : undefined;
 
-export const API_BASE = runtimeEnv || '/api';
+export const API_BASE = runtimeEnv || '';
